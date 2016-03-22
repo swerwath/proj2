a3fdf9ac4e6f82b807a39adda9294a944931fc4e
swerwath <swerwath@berkeley.edu>
2016-03-22 16:31:52 -0400

    Non signed-in users redirect to sign-in page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # If we aren't signed in, redirect to our sign in page
+    if not user_signed_in?
+      redirect_to new_user_session_path
+    end
+
   end
 end
