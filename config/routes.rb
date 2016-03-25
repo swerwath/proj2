75510211136295c157476a8580bf01084b4855cd
swerwath <swerwath@berkeley.edu>
2016-03-24 22:11:59 -0400

    Added resource routes for clubs and teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :clubs
+  resources :teams
   root to: "home#index"
 end
--- db/migrate/20160322023833_create_users_teams.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersTeams < ActiveRecord::Migration
-  def change
-    create_table :users_teams, id: false do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :team, index: true
-    end
-  end
-end
