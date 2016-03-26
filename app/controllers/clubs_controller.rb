86a8b8bbb779ccd386d5c9b4401855f1a861e8cf
swerwath <swerwath@berkeley.edu>
2016-03-25 23:13:27 -0400

    Added ability to join open club, viewing club

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,12 +1,24 @@
-class UsersController < ApplicationController
+class ClubsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = user.all
+    @clubs = Club.all
   end
 
   def show
-    @user = user.find(params[:id])
+    @club = Club.find(params[:id])
+  end
+
+  def join
+    club = Club.find(params[:id])
+    if not club.closed
+      general_team = Team.where(club_id: club.id, name: "general")
+      current_user.teams.append(general_team)
+      redirect_to "/clubs/#{club.id}"
+    end
+    else
+      redirect_to root_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :clubs
   resources :teams
   resources :announcements
   resources :comments
+
   root to: "home#index"
+
+  patch 'join/:id', to: 'clubs#join', as: 'join'
+
 end
