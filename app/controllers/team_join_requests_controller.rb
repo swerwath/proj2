9e7c7f5172d4f73daf190f620e3e56fb036638f8
swerwath <swerwath@berkeley.edu>
2016-03-25 23:57:48 -0400

    Added controller methods for requesting/destroying join requests

--- app/controllers/team_join_requests_controller.rb
+++ app/controllers/team_join_requests_controller.rb
@@ -3,4 +3,18 @@ class TeamJoinRequestsController < ApplicationController
     TeamJoinRequest.create user_id: current_user.id, team_id: params[:id]
     redirect_to root
   end
+
+  def approve
+    request = TeamJoinRequest.find(params[:id])
+    user = request.user
+    team_to_join = request.team
+    user.teams.append(team_to_join)
+    request.destroy
+  end
+
+  def destroy
+    request = TeamJoinRequest.find(params[:id])
+    request.destroy
+  end
+
 end
