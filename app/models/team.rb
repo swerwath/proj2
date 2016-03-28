4eae05f97101a5cc83030a37801a255b9d960a26
swerwath <swerwath@berkeley.edu>
2016-03-27 23:05:58 -0700

    Added feed method to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,8 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   has_many :team_join_requests
+
+  def feed(start=0, end=self.announcements.size)
+    self.announcements[start...end]
+  end
 end
