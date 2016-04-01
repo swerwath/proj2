aa4e05621661db332083f72c7c4b147243a8b4a5
swerwath <swerwath@berkeley.edu>
2016-04-01 00:28:11 -0700

    Empty comments no longer allowed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     session[:return_to] = request.referer
-    c = Comment.create user_id: current_user.id, announcement_id: params[:comment][:announcement_id], content: params[:comment][:content]
+    @c = Comment.new user_id: current_user.id, announcement_id: params[:comment][:announcement_id], content: params[:comment][:content]
+    if not @c.save
+        flash[:error] = @c.errors.full_messages.to_sentence        
+    end
     redirect_to session.delete(:return_to)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :announcement
+    validates :content, presence: {message: " is required for every comment."}
+
+    belongs_to :user
+    belongs_to :announcement
 end
