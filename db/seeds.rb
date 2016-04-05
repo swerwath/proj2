56f6bc9c8db5985e2bebc153ffe24c81933fde5c
swerwath <swerwath@berkeley.edu>
2016-04-05 10:29:00 -0700

    Modified seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Make Clubs
-%w(Service Sports Computers).each do |name|
+%w(Service Sports Computers Debate).each do |name|
   c = Club.create name: name, description: name+" Club", closed: false
   t = Team.create name: "general", description: "General discussion for "+name, leader_id: c.id, closed: false
-  t2 = Team.create name: "fundraising", description: "Fundraising is fun(d)!", leader_id: c.id, closed: false
+  t2 = Team.create name: "fundraising", description: "Fundraising is fun(d)!", leader_id: c.id, closed: (rand(2) == 1)
   t.club = c
   t2.club = c
   t.save
   t2.save
 end
 
-%w(John Cathy Will Jill).each do |name|
+%w(John Cathy Will Jill Chris).each do |name|
   u = User.create name: name, email: name+"@gmail.com", password: 'password'
   t = Team.find_by_id((u.id - 1) * 2 + 1)
   t2 = Team.find_by_id(t.id + 1)
