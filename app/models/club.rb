8cb88504eecee0e3c1f3b433cf08023b74ef7274
swerwath <swerwath@berkeley.edu>
2016-04-15 21:46:25 -0700

    Added members method to club

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,4 +4,13 @@ class Club < ActiveRecord::Base
   has_many :club_presidents
   has_many :officers, :through => :club_officers, :source => :user
   has_many :presidents, :through => :club_presidents, :source => :user
+
+  def members
+    members = []
+    self.teams.each do |t|
+      members += t.users
+    end
+    members.uniq
+  end
+
 end
