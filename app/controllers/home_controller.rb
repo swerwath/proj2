6020f8ba18499f4f86da4e41cf806345396a1bc7
swerwath <swerwath@berkeley.edu>
2016-04-16 22:08:57 -0700

    Added landing page redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # If we aren't signed in, redirect to our sign in page
     if not user_signed_in?
-      redirect_to new_user_session_path
+      redirect_to landing_path
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'teams/new', to: 'teams#new', as: 'create_team'
+  get 'landing', to: 'home#landing', as: 'landing'
 
   patch 'destroy_announcement/:id', to: 'announcements#destroy', as: 'destroy_announcement'
   patch 'destroy_comment/:id', to: 'comments#destroy', as: 'destroy_comment'
