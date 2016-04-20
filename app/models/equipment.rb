64cfe19a448595e8d29260c984928bd0a88aea7b
swerwath <swerwath@berkeley.edu>
2016-04-19 23:12:16 -0700

    Added validations for equipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,6 @@
 class Equipment < ActiveRecord::Base
+  validates :name, presence: {message: " is missing. Equipment needs a name."}
+  validates :condition, presence: {message: " is missing. Equipment needs a condition."}
 
   belongs_to :user
   belongs_to :team
